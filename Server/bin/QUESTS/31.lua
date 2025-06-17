a09afe225c37a013143d23abf4edd0094f62e153
twostars <i.am.twostars@gmail.com>
2025-06-18 05:16:08 +1000

    Strip Lua scripts
    The EVTs will be used instead.

--- Server/bin/QUESTS/31.lua
+++
@@ -1,7 +0,0 @@
-if nEventID == 22001 then
-	pUser:SelectMsg(51000, 50000, 22010, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
-elseif nEventID == 22010 then
-	pUser:ZoneChange(21, 306, 352);
-else
-	pUser:SendDebugString("Unprocessed event id ("..nEventID..")");
-end
